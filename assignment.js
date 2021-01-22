c007637870b04962b0eb9ab886814aa8272133cd
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-01-22 18:35:51 +0600

    Completed: Assignment

--- assignment.js
+++ assignment.js
@@ -22,5 +22,50 @@ function budgetCalculator(numberOfWatchToBuy, numberOfPhoneToBuy, numberOfLaptop
 
   //calculate and return total price
   return (watchPrice * numberOfWatchToBuy) + (phonePrice * numberOfPhoneToBuy) + (laptopPrice * numberOfLaptopToBuy);
-
 }
+
+
+// hotel cost calculator
+// takes one parameter and return the total cost according to parameter
+function hotelCost(numberOfDays) {
+  // validations
+  if (numberOfDays == null) return 'Value can not be empty';
+  if (numberOfDays < 0 || typeof numberOfDays != "number") return 'Invalid parameter value';
+
+  let roomRent = 0;
+  let firstTenDaysRent = 0;
+  let secondTenDaysRent = 0;
+  let remainingDaysRent = 0;
+
+  if (numberOfDays <= 10) {
+    roomRent = 100 * numberOfDays;
+  } else if (numberOfDays <= 20) {
+    firstTenDaysRent = 10 * 100;
+    remainingDaysRent = (numberOfDays - 10) * 80;
+    roomRent = firstTenDaysRent + remainingDaysRent;
+  } else {
+    firstTenDaysRent = 10 * 100;
+    secondTenDaysRent = 10 * 80;
+    remainingDaysRent = numberOfDays - 20;
+    roomRent = firstTenDaysRent + secondTenDaysRent + remainingDaysRent * 50
+  }
+  return roomRent;
+}
+
+// recives an array and return the max length element
+function megaFriend(numberOfFriends) {
+  if (numberOfFriends.length < 0) return 'Length can not be empty';
+  let maxLengthElement = numberOfFriends[0];
+
+  for (let i = 1; i < numberOfFriends.length; i++) {
+    let element = numberOfFriends[i];
+
+    // update the value
+    if (element.length > maxLengthElement.length) {
+      maxLengthElement = element;
+    }
+  }
+  return maxLengthElement;
+}
+
+
